98c61cd7a82e8b0c1e7f01e9bc1f64c7fdd42c14
walter pereira <valente.valente7@gmail.com>
2023-05-22 16:19:13 -0300

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end
+
+end
